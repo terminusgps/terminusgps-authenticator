b05bb0239d297cd716a6883f18a08eada126a5ed
darthnall <blake@proton.me>
2025-03-11 15:33:09 -0500

    Removed old template and updated docs

--- terminusgps_authenticator/validators.py
+++ terminusgps_authenticator/validators.py
@@ -1,7 +1,7 @@
 from django.contrib.auth import get_user_model
 from django.core.files import File
 from django.core.validators import FileExtensionValidator
-from django.db.models import Q
+from django.db.models import Q, QuerySet
 from django.forms import ValidationError
 from django.utils.translation import gettext_lazy as _
 
@@ -9,7 +9,9 @@ from django.utils.translation import gettext_lazy as _
 def validate_spreadsheet_file(value: File) -> None:
     """Raises :py:exec:`~django.core.exceptions.ValidationError` if the file is not a spreadsheet file."""
     try:
-        validator = FileExtensionValidator(allowed_extensions=["csv", "xlsx"])
+        validator: FileExtensionValidator = FileExtensionValidator(
+            allowed_extensions=["csv", "xlsx"]
+        )
         validator(value)
     except ValidationError:
         raise
@@ -17,8 +19,8 @@ def validate_spreadsheet_file(value: File) -> None:
 
 def validate_email_unique(value: str) -> None:
     """Raises :py:exec:`~django.core.exceptions.ValidationError` if the email is non-unique among Django usernames."""
-    filters = Q(username__iexact=value) | Q(email__iexact=value)
-    queryset = get_user_model().objects.filter(filters)
+    filters: Q = Q(username__iexact=value) | Q(email__iexact=value)
+    queryset: QuerySet = get_user_model().objects.filter(filters)
 
     if queryset.exists():
         raise ValidationError(
